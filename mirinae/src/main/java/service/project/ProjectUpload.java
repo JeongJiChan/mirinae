9c8dd420899529001affb6f0c58030745e996889
tjdgu <tjdgu@DESKTOP-7TH0JT0>
2021-11-19 17:27:31 +0900

    21. 11. 19. seong
    옵션 한개 추가 가능
    이미지파일 프로젝트번호로 이름 바꿔서 폴더에 저장

--- mirinae/src/main/java/service/project/ProjectUpload.java
+++ mirinae/src/main/java/service/project/ProjectUpload.java
@@ -2,11 +2,16 @@
 
 package service.project;
 
+import java.io.File;
 import java.sql.Date;
 
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 import dao.ProjectDao;
 import dao.MemberDao;
@@ -20,39 +25,26 @@ public class ProjectUpload implements CommandProcess {
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) {
+		String real = request.getSession().getServletContext().getRealPath("/p_images");
+		int maxSize = 1024 * 1024 * 10;
+		try {
+		MultipartRequest mr = new MultipartRequest(request, real, maxSize,
+				"utf-8", new DefaultFileRenamePolicy());
 		
-		HttpSession session = request.getSession();
-		
-		int cate_code = Integer.parseInt(request.getParameter("category"));
-		String p_name = request.getParameter("name");
-		String p_content = request.getParameter("content");
+	//	HttpSession session = request.getSession();
+
+		int cate_code = Integer.parseInt(mr.getParameter("category"));
+		String p_name = mr.getParameter("name");
+		String p_content = mr.getParameter("content");
 	//	String m_id = (String)session.getAttribute("id");
 	//	MemberDao memberDao = MemberDao.getInstance();
 	//  Member member = memberDao.select(m_id);
 	//	String p_writer = member.getM_name();
-		Date s_date = Date.valueOf(request.getParameter("s_date"));
-		Date e_date = Date.valueOf(request.getParameter("e_date"));
-		int goal_money = Integer.parseInt(request.getParameter("goal_money"));
-//		int p_view 조회수
-//		String p_del 삭제 여부
-//		Date reg_date 작성일자
-//		int cur_money 현재 달성 금액
-		
-//		여러데이터를 구현해야함
-	 	String opt_name1 = request.getParameter("name_arr");
-	 	String opt_price1 = request.getParameter("price_arr");
-	 	String[] price = opt_price1.split(",");
-	 	String[] name = opt_name1.split(",");
-		String[] opt_name = new String[name.length];
-		for(int i =0; i < name.length; i++) {
-			opt_name[i] = name[i];
-		} 
-	 	int[] opt_price = new int[price.length];
-		for(int i =0; i < price.length; i++) {
-			opt_price[i] = Integer.parseInt(price[i]);
-		}  
- 
+		Date s_date = Date.valueOf(mr.getParameter("s_date"));
+		Date e_date = Date.valueOf(mr.getParameter("e_date"));
+		int goal_money = Integer.parseInt(mr.getParameter("goal_money"));
 
+	//  프로젝트 셋팅
 		Project project = new Project();
 		project.setCate_code(cate_code);
 		project.setP_name(p_name);
@@ -64,30 +56,73 @@ public class ProjectUpload implements CommandProcess {
 		
 		int result = 0;
 		ProjectDao pd = ProjectDao.getInstance();
+		//프로젝트 넘버 알아오기
 		int p_no = pd.find_p_no();
 		project.setP_no(p_no);
 		int resultpro = pd.insert(project); 
 		
+		
+		//옵션 세팅
 		int resultopt = 0;
-
+	 	String opt_name1 = mr.getParameter("name_arr");
+	 	String opt_price1 = mr.getParameter("price_arr");
 		OptionsDao od = OptionsDao.getInstance();
+		//옵션 코드 알아오기
 		int opt_code = od.find_opt_code();
-		for(int i = 0; i < opt_price.length; i++) {
+	 	if(opt_name1.contains(",")) {
+	 		//옵션 다중 저장
+		 	String[] price = opt_price1.split(",");
+		 	String[] name = opt_name1.split(",");
+			String[] opt_name = new String[name.length];
+			for(int i =0; i < name.length; i++) {
+				opt_name[i] = name[i];
+			} 
+		 	int[] opt_price = new int[price.length];
+			for(int i =0; i < price.length; i++) {
+				opt_price[i] = Integer.parseInt(price[i]);
+			}  
+			for(int i = 0; i < opt_price.length; i++) {
+				Options options = new Options();
+				options.setOpt_code(opt_code);
+				options.setOpt_name(opt_name[i]);
+				options.setOpt_price(opt_price[i]);
+				options.setP_no(p_no);
+				resultopt = od.insert(options);
+				opt_code++;
+			}
+	 	}else {
+	 		//옵션 한 건 저장
+	 		String opt_name = opt_name1;
+	 		int opt_price = Integer.parseInt(opt_price1);
 			Options options = new Options();
 			options.setOpt_code(opt_code);
-			options.setOpt_name(opt_name[i]);
-			options.setOpt_price(opt_price[i]);
+			options.setOpt_name(opt_name);
+			options.setOpt_price(opt_price);
 			options.setP_no(p_no);
-			resultopt = od.insert(options); 
-			opt_code++;
-		}
-		
+			resultopt = od.insert(options);
+	 	}
+	
+	 	//파일 이름 변경
+	 	 
+ 	    String fileName = mr.getFilesystemName("filename");
+ 	    String now = String.valueOf(p_no);	 	 
+ 	    int i = -1;
+ 	   	i = fileName.lastIndexOf("."); // 파일 확장자 위치
+ 	   	//이름과 확장자 결합
+ 	   	String realFileName = now + fileName.substring(i, fileName.length());     
+ 	    File oldFile = new File(real +"/"+ fileName);
+ 	    File newFile = new File(real +"/"+ realFileName);
+	 	   
+ 	    oldFile.renameTo(newFile); // 파일명 변경
+
 		if(resultpro == 1 && resultopt == 1) {
 			result = 1;
 		}
-			
-		request.setAttribute("result", result);
+	
+		 request.setAttribute("result", result);
+		}catch (Exception e) {
+			System.out.println("연결에러 : "+e.getMessage());
+		}
 		return "/project/p_upload";
 	}
-
 }
