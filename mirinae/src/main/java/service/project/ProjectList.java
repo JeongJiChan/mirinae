7f60f1fb0fa755c4d38e70674caba8a2ff9fd126
tjdgu <tjdgu@DESKTOP-7TH0JT0>
2021-11-22 18:49:34 +0900

    21. 11. 22. seong 프로젝트 등록누르면 비로그인시 로그인 폼으로 보내기

--- mirinae/src/main/java/service/project/ProjectList.java
+++ mirinae/src/main/java/service/project/ProjectList.java
@@ -14,7 +14,8 @@ public class ProjectList implements CommandProcess {
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) {
 		ProjectDao pd = ProjectDao.getInstance();
 		List<Project> project = pd.selectList();
-
+		
+		
 		request.setAttribute("project", project);
 		return "/project/p_list";
 	}
