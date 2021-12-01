20093d56677d366364cb126d677fd89533dc3348
onlot <alsrnjs419@gmail.com>
2021-12-01 15:13:04 +0900

    회원탈퇴/관리자페이지에서 회원 강퇴할 때 등록한 프로젝트 있으면 막음

--- mirinae/src/main/java/service/member/Delete.java
+++ mirinae/src/main/java/service/member/Delete.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import dao.MemberDao;
+import dao.ProjectDao;
+import model.Member;
 import service.main.CommandProcess;
 
 public class Delete implements CommandProcess {
@@ -11,10 +13,15 @@ public class Delete implements CommandProcess {
 		HttpSession session = request.getSession();
 		String id = (String)session.getAttribute("id");
 		MemberDao md = MemberDao.getInstance();
-		int result = md.delete(id);
-		if(result > 0) {
+		ProjectDao pd = ProjectDao.getInstance();
+		
+		int result = 0;
+		
+		if(pd.idSearch(id) == 0) {
+			result = md.delete(id);
 			session.invalidate();
 		}
+		System.out.println(result);
 		request.setAttribute("result", result);
 		return "/member/delete";
 	}
